3176429d073efb66d2e9bfb90472a7ea3084728e
Andrey621 <achernyavsky10@gmail.com>
2023-04-24 23:18:37 +0300

    Initial commit 24/04/2023 23-18

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for creating a recommender system for drugs (data science cookiecutter repo)',
+    author='Andrey621',
+    license='MIT',
+)
